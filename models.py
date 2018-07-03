b3ec52614fc432d0720888b5f213a2bd39ac3bb8
Joe Neylon <jjneylon@gmail.com>
2018-07-03 13:54:07 -0500

    gh-1: added Site model

--- models.py
+++ models.py
@@ -2,9 +2,17 @@ from service import db
 from sqlalchemy_utils import UUIDType
 
 
+class Site(db.Model):
+    uuid = db.Column(UUIDType(), primary_key=True)
+    chapter_uuid = db.Column(UUIDType())
+    title = db.Column(db.String(32))
+    latitude = db.Column(db.String(32))
+    longitude = db.Column(db.String(32))
+
+
 class Event(db.Model):
     uuid = db.Column(UUIDType(), primary_key=True)
-    site_uuid = db.Column(UUIDType())
+    site_uuid = db.Column(UUIDType(), db.ForeignKey('Site.uuid'))
     chapter_uuid = db.Column(UUIDType())
     title = db.Column(db.String(32))
     start_time = db.Column(db.DateTime(timezone=True))
